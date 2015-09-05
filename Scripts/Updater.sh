8600626b6bd8ad43231c09ffc5009c7f214b7f94
sergio-lazic <pi@raspberrypi.(none)>
2015-09-05 15:14:10 +0000

    totaly fixed Updater.sh

--- Scripts/Updater.sh
+++ Scripts/Updater.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+cd /home/pi/Documents/Inkubator/
+
 var=$(git pull)
+
+
+
 echo $var
 if [[ $var == *"ArduinoInkubator"* ]]; then
-	/home/pi/Documents/Inkubator/Scripts/arduino-firmware-upgrate.sh
+	Scripts/arduino-firmware-upgrate.sh
 fi
