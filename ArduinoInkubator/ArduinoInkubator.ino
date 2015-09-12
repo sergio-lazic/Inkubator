f30f30d362aa1d50eef0acb817306a2fcd77bd94
pcoklo <petarcoklo@gmail.com>
2015-09-12 20:37:18 +0200

    arduino comunication part

--- ArduinoInkubator/ArduinoInkubator.ino
+++ ArduinoInkubator/ArduinoInkubator.ino
@@ -6,6 +6,9 @@
 #define DN_PIN 25          //Connect two tactile button switches (or something similar)
 #define UP_PIN 29          //from Arduino pin 2 to ground and from pin 3 to ground.
 #define OK_PIN 27
+#define BL_PIN 3
+#define HTR_PIN 12
+#define FAN_PIN 10
 #define PULLUP true        //To keep things simple, we use the Arduino's internal pullup resistor.
 #define INVERT true        //Since the pullup resistor will keep the pin high unless the
 				  //switch is closed, this is negative logic, i.e. a high state
@@ -37,17 +40,17 @@ unsigned long rpt = REPEAT_FIRST;     //A variable time that is used to drive th
 void setup(void){
 	lcd.begin(16, 2);
 	lcd.clear();
-	lcd.print("Hello bitchez!");
-	lcd.setCursor(0, 1);
-	lcd.print("lawl");
+	lcd.print("Hallo bitchez!");
 	pinMode(13, OUTPUT);
+	pinMode(BL_PIN, OUTPUT);
+	pinMode(HTR_PIN, OUTPUT);
+	pinMode(FAN_PIN, OUTPUT);
 	Serial.begin(115200);
-
 	dht.begin();
 }
 
 void loop(void){
-	if(Serial.available()) stringHandler(Serial.readStringUntil('\n'));
+	if(Serial.available()) serialHandler();
 	btnUP.read();                             //read the buttons
 	btnDN.read();
 
@@ -88,14 +91,18 @@ void loop(void){
 	}
 }
 
-void stringHandler(String data){
-	lcd.clear();
-	lcd.setCursor(0, 0);
-	lcd.print(data);
+void serialHandler(){
+	String data = Serial.readStringUntil('\n');
+	if(data.startsWith("sc")) lcd.setCursor(data.substring(data.indexOf(':'), data.indexOf(',')).toInt(), data.substring(data.indexOf(','), data.length()).toInt());
+	else if(data.startsWith("h")) digitalWrite(HTR_PIN, data.substring(data.indexOf(':'), data.length()).toInt());
+	else if(data.startsWith("f")) digitalWrite(FAN_PIN, data.substring(data.indexOf(':'), data.length()).toInt());
+	else if(data.startsWith("bl")) analogWrite(data.substring(data.indexOf(':'), data.length()), BL_PIN);
+	else if(data.startsWith("lcd")) lcd.print(data.substring(data.indexOf(':'), data.length()));
+	else if(data.startsWith("rs")) sendSensorValue();
+	else Serial.print("fof\n");
 }
 
-void readSenosrs(){
-	Serial.print(dht.readHumidity()); Serial.print("%,");
-	Serial.print(dht.readTemperature()); Serial.println("*C");
+void sendSensorValue(){
+	String data = dht.readTemperature() + "," + dht.readHumidity() + "\n":
+	Serial.print(data);
 }
-
